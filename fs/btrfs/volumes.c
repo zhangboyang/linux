63ebacbb30a426ddda2af7b7dd161a1a666fb154
Qu Wenruo <wqu@suse.com>
2022-05-22 13:47:47 +0200

    btrfs: quit early if the fs has no RAID56 support for raid56 related checks
    The following functions do special handling for RAID56 chunks:
    
    - btrfs_is_parity_mirror()
      Check if the range is in RAID56 chunks.
    
    - btrfs_full_stripe_len()
      Either return sectorsize for non-RAID56 profiles or full stripe length
      for RAID56 chunks.
    
    But if a filesystem without any RAID56 chunks, it will not have RAID56
    incompat flags, and we can skip the chunk tree looking up completely.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Anand Jain <anand.jain@oracle.com>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -5769,6 +5769,9 @@ unsigned long btrfs_full_stripe_len(struct btrfs_fs_info *fs_info,
 	struct map_lookup *map;
 	unsigned long len = fs_info->sectorsize;
 
+	if (!btrfs_fs_incompat(fs_info, RAID56))
+		return len;
+
 	em = btrfs_get_chunk_map(fs_info, logical, len);
 
 	if (!WARN_ON(IS_ERR(em))) {
@@ -5785,6 +5788,9 @@ int btrfs_is_parity_mirror(struct btrfs_fs_info *fs_info, u64 logical, u64 len)
 	struct extent_map *em;
 	struct map_lookup *map;
 	int ret = 0;
+
+	if (!btrfs_fs_incompat(fs_info, RAID56))
+		return 0;
 
 	em = btrfs_get_chunk_map(fs_info, logical, len);
 
